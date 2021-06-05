8150ed7dcede692c4081599c3f58a5d031e943b1
Oli Zimpasser <oglimmer@gmail.com>
2021-06-05 22:30:23 +0200

    improve import.sh

--- import.sh
+++ import.sh
@@ -1,10 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -eu
 
 TOTAL=${1:-1}
 
+INITIAL_ROWS=$(MYSQL_PWD=root mysql -u root --protocol=tcp -N -s -e 'select count(*) from test.person')
+
 curl http://localhost:8080 -d $TOTAL -H 'Content-Type: application/json'
+
+function bar()
+{
+    CHAR=$1
+    COUNT=$2
+    if [ "$COUNT" = "0" ]; then
+        echo "";
+    else
+        BAR="$(printf "$CHAR"'%.0s' $(seq 1 $COUNT))"
+        echo "$BAR"
+    fi
+}
 
 function progressbar()
 {
@@ -12,13 +26,14 @@ function progressbar()
     MAX_VAL=$2
     PERC=$(bc <<< "100*$CUR_VAL/$MAX_VAL")
     NOT_PERC="$((100-PERC))"
-    BAR="$(printf '*%.0s' $(seq 1 $PERC))$(printf ' %.0s' $(seq 1 $NOT_PERC))"
+    BAR="$(bar '*' $PERC)$(bar ' ' $NOT_PERC)"
     echo -ne "$BAR ($PERC%)     \r"
 }
 
 ROWS=0
 while [ "$ROWS" -lt "$(( 100*TOTAL ))" ]; do
     ROWS=$(MYSQL_PWD=root mysql -u root --protocol=tcp -N -s -e 'select count(*) from test.person')
+    ROWS=$((ROWS - INITIAL_ROWS))
     progressbar "$ROWS" "$(( 100*TOTAL ))"
     sleep 1
 done
